afd4bbc5ecde8105886455489ccc7321f6a5aad3
Arijit <arijitmaiti085@gmail.com>
2021-12-28 11:24:28 +0530

    server js file added

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist/portfolio'));
+
+app.get('/*', (req, res) =>  res.sendFile('index.html', {root: 'dist/portfolio/'}));
+
+app.listen(process.env.PORT || 8080);
