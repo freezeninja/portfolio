69a8321898ae519ab52e1aad6a2b61e712643200
Arijit <arijitmaiti085@gmail.com>
2022-03-06 15:21:36 +0530

    added external links

--- src/app/portfolio/port-main/port-main.component.ts
+++ src/app/portfolio/port-main/port-main.component.ts
@@ -14,7 +14,7 @@ export class PortMainComponent implements OnInit {
 
   IsDisplayed = false;
 
-  CurrentDate: any = new Date("20 dec 2021").getTime()/ 1000;
+  CurrentDate: any = new Date("20 feb 2022").getTime()/ 1000;
   futureDate:any = new Date().getTime()/1000;
   getDate(){
     let b = this.futureDate - this.CurrentDate;
@@ -38,68 +38,79 @@ export class PortMainComponent implements OnInit {
       title: 'Logibook(old)',
       url: 'assets/img/logibbokOrange.png',
       desc: 'Single Page Logistic Website.',
-      category: 'bootstrap'
+      category: 'bootstrap',
+      src: 'https://peepsenpai.github.io/logibook/'
     },
     {
       title: 'Logibook(New)',
       url: 'assets/img/logibookBlue.png',
       desc: 'Client wanted to redesign the whole site with a new look.',
-      category: 'bootstrap'
+      category: 'bootstrap',
+      src: 'https://www.logibook.in/'
     },
     {
       title: 'Fashion Ecommerce',
       url: 'assets/img/portEcomerce.png',
       desc: 'Created this clothes shop just for practicing purpose.',
-      category: 'bootstrap'
+      category: 'bootstrap',
+      src: 'https://peepsenpai.github.io/ecommerce1/'
     },
     {
       title: 'AC Store',
       url: 'assets/img/electrico.png',
       desc: 'An ecommerce website for selling air conditioners. Whole site, every single page made by me from scratch',
-      category: 'bootstrap'
+      category: 'bootstrap',
+      src: 'https://electricoworld.com/'
     },
     {
       title: 'Login - Signup',
       url: 'assets/img/wlogin.png',
       desc: 'These page made without any external library and have some animation.',
-      category: 'WEXL'
+      category: 'WEXL',
+      src: 'https://peepsenpai.github.io/signinpage/'
     },
     {
       title: 'Telecom Site',
       url: 'assets/img/wtelenor.png',
       desc: 'This site was made for a Norway Client.',
-      category: 'WEXL'
+      category: 'WEXL',
+      src: 'https://peepsenpai.github.io/telenor/'
     },
     {
       title: 'Weather App',
       url: 'assets/img/wweather.png',
       desc: 'Fetch data from openweather.com and displayed.',
-      category: 'VJS'
-    },
-    {
-      title: 'Telenor Product',
-      url: 'assets/img/wtproduct.png',
-      desc: 'Product Details Page of Telenor Website.',
-      category: 'WEXL'
+      category: 'VJS',
+      src: 'https://peepsenpai.github.io/weatherapp/'
     },
     {
       title: 'Password Generator',
       url: 'assets/img/passGen.png',
       desc: 'Generate a strong password and use it.',
-      category: 'angular'
+      category: 'angular',
+      src: 'https://password-generator-beta-wine.vercel.app/'
     },
     {
       title: 'Todo List',
       url: 'assets/img/todo.png',
       desc: 'Add your ToDo list items and track your incomplete task.',
-      category: 'VJS'
+      category: 'VJS',
+      src: 'https://peepsenpai.github.io/MyList/'
     },
     {
       title: 'Image Search',
       url: 'assets/img/simage.png',
       desc: 'Search images with the help of Unsplash API, Day - Night toggle botton present.',
       category: 'VJS'
-    }
+    },
+    {
+      title: 'Pocket Services',
+      url: 'assets/img/pocket_services.png',
+      desc: 'Make QR Code , Bar code and many more thing within a minute',
+      category: 'react',
+      src: 'https://textutil-umber.vercel.app/'
+    },
+
   ]
 
   constructor() { }
